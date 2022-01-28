635452447a35d8c660f334941d64796c9f5a3f53
unknown <mattenos4@gmail.com>
2022-01-27 20:45:51 -0500

    created the tables in schema.

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
-DROP DATABASE IF EXISTS grocery_db;
-CREATE DATABASE grocery_db;
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
 
-USE grocery_db;
+USE business_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name: VARCHAR(30)
+    id INT PRIMARY KEY,
+    name VARCHAR(30) 
 );
 
 CREATE TABLE role (
-    id: INT PRIMARY KEY,
-    title: VARCHAR(30),
-    salary: DECIMAL,
-    department_id: INT
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
 );
 
 CREATE TABLE employee (
-    id: INT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT,
-    manager_id: INT
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
 );
